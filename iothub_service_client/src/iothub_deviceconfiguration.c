99201235204ec77f60e6f8b10b2b1c718ff13a07
Rajeev Massand <rajeev.massand@microsoft.com>
2018-05-31 16:17:33 -0700

    Add scaffolding for configuration API

---
+++ iothub_service_client/src/iothub_deviceconfiguration.c
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "iothub_deviceconfiguration.h"
+
+IOTHUB_SERVICE_CLIENT_DEVICE_CONFIGURATION_HANDLE IoTHubDeviceConfiguration_Create(IOTHUB_SERVICE_CLIENT_AUTH_HANDLE serviceClientHandle)
+{
+    return NULL;
+}
+
+void IoTHubDeviceConfiguration_Destroy(IOTHUB_SERVICE_CLIENT_DEVICE_CONFIGURATION_HANDLE serviceClientDeviceConfigurationHandle)
+{
+}
+
+IOTHUB_DEVICE_CONFIGURATION_RESULT IoTHubDeviceConfiguration_GetConfiguration(IOTHUB_SERVICE_CLIENT_DEVICE_CONFIGURATION_HANDLE serviceClientDeviceConfigurationHandle, const char* configurationId, IOTHUB_DEVICE_CONFIGURATION* configuration)
+{
+    return IOTHUB_DEVICE_CONFIGURATION_OK;
+}
+
+IOTHUB_DEVICE_CONFIGURATION_RESULT IoTHubDeviceConfiguration_AddConfiguration(IOTHUB_SERVICE_CLIENT_DEVICE_CONFIGURATION_HANDLE serviceClientDeviceConfigurationHandle, const IOTHUB_DEVICE_CONFIGURATION_CREATE* configurationCreate, IOTHUB_DEVICE_CONFIGURATION* configuration)
+{
+    return IOTHUB_DEVICE_CONFIGURATION_OK;
+}
+
+IOTHUB_DEVICE_CONFIGURATION_RESULT IoTHubDeviceConfiguration_UpdateConfiguration(IOTHUB_SERVICE_CLIENT_DEVICE_CONFIGURATION_HANDLE serviceClientDeviceConfigurationHandle, const IOTHUB_DEVICE_CONFIGURATION_UPDATE* configurationUpdate, IOTHUB_DEVICE_CONFIGURATION* configuration)
+{
+    return IOTHUB_DEVICE_CONFIGURATION_OK;
+}
+
+IOTHUB_DEVICE_CONFIGURATION_RESULT IoTHubDeviceConfiguration_DeleteConfiguration(IOTHUB_SERVICE_CLIENT_DEVICE_CONFIGURATION_HANDLE serviceClientDeviceConfigurationHandle, const char* configurationId)
+{
+    return IOTHUB_DEVICE_CONFIGURATION_OK;
+}
---
+++ iothub_service_client/tests/iothub_deviceconfiguration_ut/iothub_deviceconfiguration_ut.c
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "testrunnerswitcher.h"
+
+#include "iothub_deviceconfiguration.h"
+
+BEGIN_TEST_SUITE(iothub_devicetwin_ut)
+
+TEST_SUITE_INITIALIZE(TestClassInitialize)
+{
+}
+
+TEST_SUITE_CLEANUP(TestClassCleanup)
+{
+}
+
+TEST_FUNCTION_INITIALIZE(TestMethodInitialize)
+{
+}
+
+TEST_FUNCTION_CLEANUP(TestMethodCleanup)
+{
+}
+
+TEST_FUNCTION(Test)
+{
+}
+
+END_TEST_SUITE(iothub_devicetwin_ut)
